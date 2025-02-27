b57c2aead9ea1f656a37c9e19cfe91cdcc279440
cawzkf <camilafelixdosreis@gmail.com>
2025-02-27 13:46:01 -0400

    Ajustes layout,scanActivity e xml, item_planta e item_linha

--- app/src/main/java/com/ecoactivity/caica/initActivity.kt
+++ app/src/main/java/com/ecoactivity/caica/initActivity.kt
@@ -1,6 +1,8 @@
 package com.ecoactivity.caica
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,24 @@ class initActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val abrirCatalago = findViewById<Button>(R.id.botao_catalogo)
+
+        val abrirScan = findViewById<Button>(R.id.botao_escanear)
+
+        abrirScan.setOnClickListener {
+            try {
+                val intent = Intent(this, scanActivity::class.java)
+                startActivity(intent)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        abrirCatalago.setOnClickListener {
+            val intent = Intent(this,CatalogoActivity::class.java)
+            startActivity(intent)
         }
     }
 }
