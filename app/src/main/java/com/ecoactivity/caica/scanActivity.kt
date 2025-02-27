1c5403e922f05e2a347c9c9c0f63c6b8084db272
cawzkf <camilafelixdosreis@gmail.com>
2025-02-27 13:46:01 -0400

    Ajustes layout,scanActivity e xml, item_planta e item_linha

--- app/src/main/java/com/ecoactivity/caica/scanActivity.kt
+++ app/src/main/java/com/ecoactivity/caica/scanActivity.kt
@@ -1,5 +1,14 @@
 package com.ecoactivity.caica
 
-class scanActivity {
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class scanActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan)
+
+    }
 }
