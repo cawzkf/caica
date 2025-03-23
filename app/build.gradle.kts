12c4adb0c3cd271bb8069a496fe89d05a3aecac9
cawzkf <camilafelixdosreis@gmail.com>
2025-03-23 00:18:26 -0400

    Importação do glide, Card com exemplos e novo formato + visualizacao das listas com dados teste, ajuste da conexao com banco + teste com dados declarados, novo botao de capturar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
 }
 
 android {
@@ -54,5 +55,6 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
     implementation(libs.kotlinx.coroutines.android)
-
+    implementation(libs.glide)
+    kapt(libs.glide.compiler)
 }
